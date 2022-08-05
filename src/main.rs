b643a9f46948c6544f515f2e758e610f75f1816b
Jelle Haandrikman <jhaand@freedom.nl>
2022-08-05 23:51:29 +0200

    Getting to display, but not yet.

--- src/main.rs
+++ src/main.rs
@@ -1,23 +1,22 @@
-#![allow(unused_imports)]
-#![allow(clippy::single_component_path_imports)]
-
 mod wifi_creds;
+
+use core::fmt::Debug;
+/* 
 use std::fs;
 use std::io::{Read, Write};
 use std::net::{TcpListener, TcpStream};
 use std::path::PathBuf;
 use std::sync::{Condvar, Mutex};
 use std::{cell::RefCell, env, sync::atomic::*, sync::Arc, thread, time::*};
+*/
 
-use anyhow::bail;
-
-use embedded_svc::mqtt::client::utils::ConnState;
+// use embedded_svc::mqtt::client::utils::ConnState;
+// use url;
 use log::*;
 
-use url;
 
-use smol;
-
+// use smol;
+/*
 use embedded_hal::adc::OneShot;
 use embedded_hal::blocking::delay::DelayMs;
 use embedded_hal::digital::v2::OutputPin;
@@ -50,19 +49,21 @@ use esp_idf_svc::wifi::*;
 
 use esp_idf_hal::adc;
 use esp_idf_hal::delay;
+*/ 
 use esp_idf_hal::gpio;
 use esp_idf_hal::i2c;
 use esp_idf_hal::prelude::*;
 use esp_idf_hal::spi;
 
-use esp_idf_sys::{self, c_types};
-use esp_idf_sys::{esp, EspError};
+// use esp_idf_sys::{self, c_types};
+// use esp_idf_sys::{esp, EspError};
+use esp_idf_sys as _;
 
 use display_interface_spi::SPIInterfaceNoCS;
 
+use embedded_graphics::prelude::*;
 use embedded_graphics::mono_font::{ascii::FONT_10X20, MonoTextStyle};
 use embedded_graphics::pixelcolor::*;
-use embedded_graphics::prelude::*;
 use embedded_graphics::primitives::*;
 use embedded_graphics::text::*;
 use embedded_text::{
@@ -72,20 +73,13 @@ use embedded_text::{
 };
 
 use ili9341;
-use st7789;
 
-use epd_waveshare::{epd4in2::*, graphics::VarDisplay, prelude::*};
+// use epd_waveshare::{epd4in2::*, graphics::VarDisplay, prelude::*};
 
 #[allow(dead_code)]
 const SSID: &str = wifi_creds::SSID;
 #[allow(dead_code)]
 const PASS: &str = wifi_creds::PASS;
-
-#[cfg(esp32s2)]
-include!(env!("EMBUILD_GENERATED_SYMBOLS_FILE"));
-
-#[cfg(esp32s2)]
-const ULP: &[u8] = include_bytes!(env!("EMBUILD_GENERATED_BIN_FILE"));
 
 thread_local! {
     static TLS: RefCell<u32> = RefCell::new(13);
@@ -96,27 +90,15 @@ fn main() -> Result<()> {
 
     test_print();
 
-    #[cfg(not(esp_idf_version = "4.3"))]
-    test_fs()?;
-    esp_idf_svc::log::EspLogger::initialize_default();
-
     #[allow(unused)]
     let peripherals = Peripherals::take().unwrap();
     #[allow(unused)]
     let pins = peripherals.pins;
 
-    let mut display_control = pins.gpio26.into_output().unwrap();
-    display_control.set_low();
+    let mut display_control = pins.gpio26.into_output()?;
+    display_control.set_low()?;
 
-
-    #[allow(unused)]
-    let netif_stack = Arc::new(EspNetifStack::new()?);
-    #[allow(unused)]
-    let sys_loop_stack = Arc::new(EspSysLoopStack::new()?);
-    #[allow(unused)]
-    let default_nvs = Arc::new(EspDefaultNvs::new()?);
-
-    kaluga_hello_world(
+    mch_hello_world(
         pins.gpio33,
         pins.gpio25,
         peripherals.spi3,
@@ -158,33 +140,7 @@ fn test_fs() -> Result<()> {
     Ok(())
 }
 
-fn test_tcp() -> Result<()> {
-    info!("About to open a TCP connection to 1.1.1.1 port 80");
-
-    let mut stream = TcpStream::connect("one.one.one.one:80")?;
-
-    let err = stream.try_clone();
-    if let Err(err) = err {
-        info!(
-            "Duplication of file descriptors does not work (yet) on the ESP-IDF, as expected: {}",
-            err
-        );
-    }
-
-    stream.write_all("GET / HTTP/1.0\n\n".as_bytes())?;
-
-    let mut result = Vec::new();
-
-    stream.read_to_end(&mut result)?;
-
-    info!(
-        "1.1.1.1 returned:\n=================\n{}\n=================\nSince it returned something, all is OK",
-        std::str::from_utf8(&result)?);
-
-    Ok(())
-}
-
-fn kaluga_hello_world(
+fn mch_hello_world(
     dc: gpio::Gpio33<gpio::Unknown>,
     rst: gpio::Gpio25<gpio::Unknown>,
     spi: spi::SPI3,
@@ -192,10 +148,6 @@ fn kaluga_hello_world(
     sdo: gpio::Gpio23<gpio::Unknown>,
     cs: gpio::Gpio32<gpio::Unknown>,
 ) -> Result<()> {
-    info!(
-        "About to initialize the Kaluga {} SPI LED driver",
-        "ILI9341"
-    );
 
     let config = <spi::config::Config as Default>::default().baudrate((40).MHz().into());
 
@@ -288,7 +240,7 @@ where
     Ok(())
 }
 
-// Kaluga needs customized screen orientation commands
+// MCH2022 badge needs customized screen orientation commands
 // (not a surprise; quite a few ILI9341 boards need these as evidenced in the TFT_eSPI & lvgl ESP32 C drivers)
 pub enum KalugaOrientation {
     Portrait,
